c7b143b73e95d7ec4fa8d57f410586894c8ff7a8
Andrea T <tostonar@gmail.com>
2019-10-30 10:29:16 -0400

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -21,28 +21,32 @@ export default class MasterHog extends Component {
 
 
   render() {
+    const hogs = offspring.map(hog => (
+      <BabyHog eyeColor={this.state.eyeColor} key={hog.id} name={hog.name} hobby={hog.hobby} />
+    ));
+
     return (
       <div>
 
-        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
+        <input type="radio" name="eyeColor" value="blue" onChange={e => this.changeEyeColor(e)} />
         Blue<br></br>
-        <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor} />
+        <input type="radio" name="eyeColor" value="sun" onChange={e => this.changeEyeColor(e)} />
         Sun<br></br>
-        <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
+        <input type="radio" name="eyeColor" value="glowing" onChange={e => this.changeEyeColor(e)} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
+
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          { hogs }
         </ul>
 
       </div>
